57c77bf6f4a80209c89f8312c9d92e9e7186e178
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:45:53 -0300

    Initialize feature/final

---
+++ db/seeds/05_invoices.sql
@@ -0,0 +1,12 @@
+INSERT INTO invoices (reservations_id, date_generated, description)
+VALUES
+(1, '2023-06-30', 'Emily Johnson, emilyjohnson@gmail.com, Jupiter, 8000000'),
+(2, '2023-07-14', 'Benjamin Davis, bendavis2000@yahoo.com, Moon, 10000000'),
+(3, '2023-07-30', 'Ethan Williams, ethanw@gmail.com, Jupiter, 7000000'),
+(4, '2023-08-14', 'Olivia Smith, oliviajsmith@outlook.com, Venus, 8000000'),
+(1, '2023-08-25', 'Isabella Collins, izzycollins@yahoo.com, Venus, 8000000'),
+(2, '2023-08-30', 'Alexander Wright, alexw@gmail.com, Moon, 3000000'),
+(3, '2023-09-02', 'Bruce Patterson, brucep@outlook.com, Moon, 9000000'),
+(4, '2023-09-10', 'Jennifer Marshall, jennym@gmail.com, Jupiter, 2000000'),
+(1, '2023-09-15', 'Kenneth Peters, kenneth22@gmail.com, Venus, 4000000'),
+(2, '2023-09-26', 'Sandra Mulligan, sandymulligan@yahoo.com.com, Jupiter, 1000000');
