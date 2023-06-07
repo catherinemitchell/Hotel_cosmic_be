ccc426f6c9d16df49e5c5f7db104dca86de64f67
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:45:49 -0300

    Initialize feature/errorpage

---
+++ db/seeds/01_customers.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (name, email) VALUES
+('Melody Edwards', 'melody@email.com'),
+('Christopher Thornton', 'christopher@email.com'),
+('Hayley Vasquez', 'hayley@email.com'),
+('Aron Barker', 'aron@email.com');
