11928e43a6c8b8de5a6730d3211defafa4c32863
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:45:45 -0300

    Initialize feature/seeds2

---
+++ db/seeds/06_services_reserved.sql
@@ -0,0 +1,5 @@
+INSERT INTO services (name, description, image_url)
+VALUES
+('Restaurant Galactic', 'Enjoy a dining experience of the galaxies, with the rarest finds of the universe, cooked and served to perfection', 'https://example.com/restaurant1.jpg'),
+('Shop', 'Browse our gift shop to find the more rare gifts this universe has to offer', 'https://example.com/shop1.jpg'),
+('Stellar Spa', 'Relax and rejeuvenate in our soothing space, while enjoying a variety of treatments among the cosmos', 'https://example.com/spa1.jpg');
