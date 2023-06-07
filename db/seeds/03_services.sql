fdba0cf5bb9d487a309fa9e1df661fa13e385355
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:45:46 -0300

    Initialize feature/seeds_fix

---
+++ db/seeds/03_services.sql
@@ -0,0 +1,4 @@
+INSERT INTO services (name, description, image_url) VALUES
+('Spa session: Massage', 'Luxurious one hour hot stone massage', 'https://example.com/massage1.jpg'),
+('Spa session: Facial', 'A powerful age-defying skin care treatment designed to delay the visible signs of aging, formulated with marine and vegetal ingredients', 'https://example.com/facial2.jpg'),
+('Spa session: Mars Mud bath', 'Enjoy a therapeutic soak in a bath of warm mars mud to promote health and relaxation', 'https://example.com/bath1.jpg');
